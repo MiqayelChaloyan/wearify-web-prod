3f1ce033c91759d1bd54205f3dce322d2a99edbc
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-05-17 16:49:37 +0400

    added camera screnshot

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,8 +1,14 @@
+import React, { useRef } from 'react';
+
+import * as htmlToImage from 'html-to-image';
+
+import { createFileName, useScreenshot } from 'use-react-screenshot';
+
 import useTheme from 'hooks/useTheme';
 
 import { IoClose } from 'react-icons/io5';
 import { IoMdArrowRoundBack } from 'react-icons/io';
-import { BsFillCameraFill } from "react-icons/bs";
+import { BsFillCameraFill } from 'react-icons/bs';
 
 import './styles.css';
 
@@ -17,8 +23,29 @@ export default function Modal({
     const buttonHeadStyles = currentStepIndex !== 2 ? { right: 0 } : { left: '20px' };
     const buttonStyles = { color: '#FFFFFF' };
     const classNameButtons = currentStepIndex !== 2 ? 'modal-header' : 'modal-header-web-view';
+    const ref = useRef(null);
 
     const handleClose = () => document.getElementById('modal-viewer').style.display = 'none';
+
+
+    // Screnshot
+    const [image, takeScreenshot] = useScreenshot();
+
+    const takeScreenShot = async (node) => {
+        const dataURI = await htmlToImage.toJpeg(node);
+        return dataURI;
+    };
+
+    const download = (image, { name = "img", extension = "jpg" } = {}) => {
+        const a = document.createElement("a");
+        a.href = image;
+        a.download = createFileName(extension, name);
+        a.click();
+    };
+
+    const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
+
+    /////////////////////////////
 
     return (
         <div
@@ -27,7 +54,7 @@ export default function Modal({
             style={styles}
         >
             <div className='modal-dialog' role='document'>
-                <div className='modal-content'>
+                <div ref={ref} className='modal-content'>
                     {currentStepIndex !== 0 &&
                         <div className={classNameButtons} style={buttonHeadStyles}>
                             <button
@@ -49,7 +76,8 @@ export default function Modal({
                                     </button>
                                     <div className='button-line' />
                                     <button
-                                        className='modal-button-back'
+                                        onClick={downloadScreenshot}
+                                        className='camera-button'
                                     >
                                         <BsFillCameraFill color='white' />
                                     </button>
